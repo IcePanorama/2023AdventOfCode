dae647c9ea334e77a08401651c4878c6e4adf3dd
IcePanorama <thriftedroot@protonmail.com>
2024-02-07 16:19:08 -0500

    C Day 1, pt 1 working

---
+++ C/Day 1/main.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+FILE* load_file(char* filename);
+int char_to_int(char c);
+
+int
+main (int argc, char** argv)
+{
+  if (argc < 2)
+  {
+    puts("Error! Improper usage. Run this program by writting: ./main [some_input].txt");
+    exit(EXIT_FAILURE);
+  }
+
+  char *filename = argv[1];
+  FILE* file = load_file(filename);
+
+  const int MAX_LINE_LEN = 100;
+  char line[MAX_LINE_LEN];
+
+  int sum = 0;
+  while(fgets(line, MAX_LINE_LEN, file) != NULL)
+  {
+    printf("%s", line);
+
+    size_t line_len = strlen(line);
+
+    size_t start = 0;
+    size_t end = line_len - 1;
+
+    while (!(line[start] >= '0' && line[start] <= '9'))
+    {
+      start++;
+    }
+
+    while (!(line[end] >= '0' && line[end] <= '9'))
+    {
+      end--;
+    }
+
+    printf("first num: %c - %d\n", line[start], char_to_int(line[start]));
+    //sum += char_to_int(line[start]);
+    printf("last num: %c - %d\n", line[end], char_to_int(line[end]));
+    char new_num[] = { line[start], line[end], '\0'};
+    printf("New num: %s\n", new_num);
+    sum += atoi(new_num);//char_to_int(line[end]);
+  }
+
+  printf("%d\n", sum);
+
+  fclose(file);
+  return 0;
+}
+
+FILE* 
+load_file(char* filename)
+{
+  FILE *file;
+  file = fopen(filename, "r");
+  if (file == NULL)
+  {
+    printf("Error loading file: %s\n", filename);
+    exit(EXIT_FAILURE);
+  }
+
+  return file;
+}
+
+int 
+char_to_int(char c)
+{
+  return c - '0';
+}
